238ec78ee23a9cea7566c1d5f6faf7c4e257b971
Zezhou Wang <wangzezai@hotmail.com>
2022-11-22 21:06:50 -0500

    edit to abs path for bash script

--- run_uwb_localize.sh
+++ run_uwb_localize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # shellcheck disable=SC1090
-source ~/.bashrc
-nohup python ~/innov_uwb_api/localize_uwb.py > /dev/null 2>&1 &
+source /home/rutgers_cait/.bashrc
+nohup python /home/rutgers_cait/innov_uwb_api/localize_uwb.py > /dev/null 2>&1 &
