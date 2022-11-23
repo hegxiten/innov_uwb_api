0934a299a4b8d24184bfd0b9f5d9cef9e1ab9258
Zezhou Wang <wangzezai@hotmail.com>
2022-11-22 22:02:21 -0500

    remove nohup and avoid .sh

--- run_uwb_localize.sh
+++ run_uwb_localize.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# shellcheck disable=SC1090
-nohup /usr/bin/python3 /home/rutgers_cait/innov_uwb_api/localize_uwb.py > /dev/null 2>&1 &
+/usr/bin/python3 /home/rutgers_cait/innov_uwb_api/localize_uwb.py > /dev/null 2>&1 &
